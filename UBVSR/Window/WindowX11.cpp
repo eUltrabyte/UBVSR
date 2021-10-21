ca6244b17d4af1b586148980e729482c0c1bdf3b
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2021-10-21 20:57:44 +0100

    Test61

--- UBVSR/Window/WindowX11.cpp
+++ UBVSR/Window/WindowX11.cpp
@@ -32,6 +32,7 @@ void WindowX11::create()
 
 void WindowX11::update()
 {
+	XSendEvent(m_display, m_window, 0, ExposureMask, Expose);
 	XNextEvent(m_display, &m_event);
 
    //if (m_event.type == Expose)
