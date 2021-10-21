0ad65f744ee90098fa5b6a990438e8da3dd4935c
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2021-10-21 20:48:00 +0100

    Test52

--- UBVSR/Window/WindowX11.cpp
+++ UBVSR/Window/WindowX11.cpp
@@ -21,7 +21,7 @@ void WindowX11::create()
 	m_window = XCreateSimpleWindow(m_display, RootWindow(m_display, m_screen), 10, 10, get_win_width(), get_win_height(), 1,
 								   BlackPixel(m_display, m_screen), WhitePixel(m_display, m_screen));
 	XStoreName(m_display, m_window, get_win_title().c_str());
-	XSelectInput(m_display, m_window, ExposureMask | KeyPressMask);
+	XSelectInput(m_display, m_window, ExposureMask);
 	XMapWindow(m_display, m_window);
 	XFlush(m_display);
 }
@@ -42,11 +42,6 @@ void WindowX11::update()
 		);
 		XSync(m_display, 1);
 		XFlush(m_display);
-	}
-
-	if (m_event.type == KeyPress)
-	{
-		//std::quick_exit(0);
 	}
 }
 
