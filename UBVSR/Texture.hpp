d04603d99408b9c3f140e80690e39fe0ad12a6b1
Volian0 <40314913+Volian0@users.noreply.github.com>
2021-10-29 09:58:09 +0200

    linear interpolation maybe idk

--- UBVSR/Texture.hpp
+++ UBVSR/Texture.hpp
@@ -10,7 +10,7 @@ namespace ubv {
 		enum class FilteringType : std::uint8_t {
 			NEAREST,
 			LINEAR,
-            BILINEAR
+			BILINEAR
 		} m_filtering_type;
 
 		bool clamp_x = false;
@@ -29,25 +29,60 @@ namespace ubv {
 				break;
 				case FilteringType::LINEAR:
 				{
+					const float section_x = 1.0F / static_cast<float>(get_size().x);
+					const float section_y = 1.0F / static_cast<float>(get_size().y);
+					const float half_section_x = section_x / 2.0F;
+					const float half_section_y = section_y / 2.0F;
+					const float pos_x_mod = std::fmod(pos_uv.x, section_x);
+					const float pos_y_mod = std::fmod(pos_uv.x, section_y);
+					const float fraction_x = std::abs(pos_x_mod - half_section_x) / section_x;
+					const float fraction_y = std::abs(pos_y_mod - half_section_y) / section_y;
+					int x_texture_position = std::clamp<std::uint16_t>(std::uint16_t(pos_uv.x * get_size().x), 0, get_size().x - 1);
+					int y_texture_position = std::clamp<std::uint16_t>(std::uint16_t(pos_uv.y * get_size().y), 0, get_size().y - 1);
+					int x_texture_position2 = x_texture_position;
+					int y_texture_position2 = y_texture_position2;
+					if (pos_x_mod > half_section_x)
+					{
+						x_texture_position2 = ((x_texture_position2) + 1) % int(get_size().x);
+					}
+					else
+					{
+						x_texture_position2 = ((x_texture_position2) - 1) % int(get_size().x);
+					}
+					if (pos_y_mod > half_section_y)
+					{
+						y_texture_position2 = ((y_texture_position2) + 1) % int(get_size().y);
+					}
+					else
+					{
+						y_texture_position2 = ((y_texture_position2) - 1) % int(get_size().y);
+					}
+					const auto pixel11 = m_tga.get_pixel(ubv::u16vec2( x_texture_position, y_texture_position ));
+					const auto pixel21 = m_tga.get_pixel(ubv::u16vec2( x_texture_position2, y_texture_position ));
+					const auto pixel12 = m_tga.get_pixel(ubv::u16vec2( x_texture_position, y_texture_position2 ));
+					const auto pixel22 = m_tga.get_pixel(ubv::u16vec2( x_texture_position2, y_texture_position2 ));
+
+					//TODO: INTERPOLATE
+
 					std::abort(); // no i chuj
-                    return Pixel{ };
+					return Pixel{ };
 				}
 				break;
-                case FilteringType::BILINEAR:
+				case FilteringType::BILINEAR:
 				{
 					std::abort(); // no i chuj v2
-                    return Pixel{ };
+					return Pixel{ };
 				}
 				break;
 			}
-            return Pixel{ };
+			return Pixel{ };
 		}
 
 		inline Texture(std::string_view t_tga_filename, FilteringType t_filtering_type = FilteringType::LINEAR) 
-		    : m_tga{ t_tga_filename }, m_filtering_type{ t_filtering_type } {
+			: m_tga{ t_tga_filename }, m_filtering_type{ t_filtering_type } {
 			// cos
 		}
-     
+	 
 		constexpr u16vec2 get_size() const noexcept {
 			return u16vec2(static_cast<uint16_t>(m_tga.get_width()), static_cast<uint16_t>(m_tga.get_height()));
 		}
