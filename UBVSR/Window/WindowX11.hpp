b17db5e2c8d54a3553c2229598e4e9e8bc7aea45
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2021-10-31 14:08:30 +0100

    Tdstdf2527664g57768537889

--- UBVSR/Window/WindowX11.hpp
+++ UBVSR/Window/WindowX11.hpp
@@ -43,7 +43,7 @@ namespace ubv {
 			switch(m_event.type) {
 				case KeyPress:
 				case KeyRelease: [[fallthrough]]
-					printf( "KeyPress / KeyRelease: %i\n", m_event.xkey.keycode );
+					printf( KeyPress ? "KeyPress: %i\n" : "KeyRelease: %i\n", m_event.xkey.keycode );
 					if (m_event.xkey.keycode == keycode) {
 						XSendEvent(m_display, m_window, 0, ExposureMask, &m_event);
 						XNextEvent(m_display, &m_event);
