25b6e343b2b0cbb8c848473dffe98d57a3222be3
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2021-10-31 13:21:12 +0100

    Tdstdf2g

--- UBVSR/Pixel.hpp
+++ UBVSR/Pixel.hpp
@@ -5,9 +5,9 @@
 namespace ubv {
 	struct Pixel {
 		#if defined(__unix__)
-			std::uint8_t b;
-			std::uint8_t g;
-			std::uint8_t r;
+			std::uint8_t b{0};
+			std::uint8_t g{0};
+			std::uint8_t r{0};
 			std::uint8_t a{255};
 		#else
 			std::uint8_t b;
